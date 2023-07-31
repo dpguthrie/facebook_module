245b7e2d61fcd842f82ebc1f3944ba990659f5b6
Doug Guthrie <douglas.p.guthrie@gmail.com>
2023-07-31 16:53:02 -0600

    Update int_facebook_ads_agg.sql

--- models/intermediate/int_facebook_ads_agg.sql
+++ models/intermediate/int_facebook_ads_agg.sql
@@ -1,5 +1,5 @@
 select
     count(*) as total_views,
     count(distinct user) as total_users,
-    sum(time_spent) as total_time_spent,
+    sum(time_spent) as total_time_spent
 from {{ ref('stg_facebook_ads') }}
